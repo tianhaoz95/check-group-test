3948aeb1e8a4b56444732982bace119261730f2d
Tianhao Zhou <tianhaoz@umich.edu>
2022-02-24 08:08:26 +0000

    chore: test

--- scripts/create-a-ton-of-checks.sh
+++ scripts/create-a-ton-of-checks.sh
@@ -4,7 +4,6 @@ create_single_check () {
     curl \
         -X POST \
         -H "Accept: application/vnd.github.v3+json" \
-        -H 'authorization: token $GITHUB_TOKEN' \
         https://api.github.com/repos/tianhaoz95/check-group-test/check-runs \
         -d '{"name":"fake_check_$CURRENT_CHECK_ID","head_sha":"$CURRENT_SHA"}'
 }
