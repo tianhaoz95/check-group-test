1ba7eab1ec0fb027fbb4974192fa81a863e40d1c
Tianhao Zhou <tianhaoz@umich.edu>
2022-02-24 07:59:48 +0000

    chore: test

--- scripts/create-a-ton-of-checks.sh
+++ scripts/create-a-ton-of-checks.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-CURRENT_SHA=${{ github.event.workflow_run.head_commit.id }}
-
 function create_single_check {
     curl \
         -X POST \
         -H "Accept: application/vnd.github.v3+json" \
-        --header 'authorization: Bearer ${{ secrets.GITHUB_TOKEN }}' \
+        --header 'authorization: Bearer $GITHUB_TOKEN' \
         --header 'content-type: application/json' \
         https://api.github.com/repos/${{ github.repository }}/check-runs \
         -d '{"name":"fake_check_$CURRENT_CHECK_ID","head_sha":"$CURRENT_SHA"}'
